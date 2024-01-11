5491a5378e01a00e56b4220e1cb5898e714b2e15
홍길동 <@naver.com>
2024-01-11 21:09:47 +0900

    게시글 등록, 수정 화면 구성 완료

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -15,6 +15,7 @@ public class PostsApiController {
 
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        System.out.println("여기를 지나갑니다...");
         return postsService.save(requestDto);
     }
 
