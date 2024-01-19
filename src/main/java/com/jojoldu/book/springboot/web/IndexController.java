a5da2d214b2d7c72572b80a0d2cab3f50986fa69
홍길동 <@naver.com>
2024-01-19 22:44:36 +0900

    로직 변경 없느 단순 코드 수정

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -21,11 +21,9 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user){
-//    public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
 
 //        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-//        System.out.println(user);
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
